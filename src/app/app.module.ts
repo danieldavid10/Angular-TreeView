0056c271336c2cc6066b82fd8ea19fea6e19f687
Daniel-David <d.david.ar10@gmail.com>
2019-03-22 22:30:39 -0400

    Integracion de Angular-Tree

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TreeModule } from 'angular-tree-component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TreeModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
